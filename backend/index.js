4fcd6758529b83df8ccd8d0142c591805d0bed15
Iuri Moura <msiuri@outlook.com>
2022-08-11 12:17:48 -0300

    Add GET and POST
    Add GET and POST

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 
+//sinalizar q estamos usando express no body
+
+app.use(express.json());
+
 app.get("/", function (req, res) {
   res.send("Hello World");
-});
-
-app.get("/oi", function (req, res) {
-  res.send("Olá mundo");
 });
 
 const list = [
@@ -26,5 +26,25 @@ const list = [
     points: 45,
   },
 ];
+
+//Endpoint READ ALL [GET]
+app.get("/score", function (req, res) {
+  res.send(list);
+});
+
+//Endpoint CREATE - [POST]
+app.post("/score", function (req, res) {
+  //pega o item do formulário
+  const item = req.body;
+
+  //add item na lista
+  list.push({
+    id: list.length + 1,
+    name: item.name,
+    points: item.points,
+  });
+
+  res.status(200).send("OK");
+});
 
 app.listen(3000);
