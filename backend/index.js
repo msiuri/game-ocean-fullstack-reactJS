2244d8fb9cd224b2f8ad808a82343a2837087396
Iuri Moura <msiuri@outlook.com>
2022-08-18 09:30:08 -0300

    Get and Post

--- backend/index.js
+++ backend/index.js
@@ -1,50 +1,57 @@
 const express = require("express");
-const app = express();
+const { MongoClient } = require("mongodb");
+
+const url = "mongodb://localhost:27017";
+const dbName = "fullstack-ocean";
 
 //sinalizar q estamos usando express no body
 
-app.use(express.json());
+async function main() {
+  console.log("Conectando");
+  const client = await MongoClient.connect(url);
+  const db = client.db(dbName);
+  const collection = db.collection("pontuacoes");
+  const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+  console.log("Conectado...");
+  app.use(express.json());
 
-const list = [
-  {
-    id: 1,
-    name: "Alain",
-    points: 25,
-  },
-  {
-    id: 2,
-    name: "Beatriz",
-    points: 50,
-  },
-  {
-    id: 3,
-    name: "Daniel",
-    points: 45,
-  },
-];
+  // [GET] ALL
+  app.get("/score", async function (req, res) {
+    const item = await collection
+      .find()
+      .sort({ points: 1 })
+      .limit(10)
+      .toArray();
 
-//Endpoint READ ALL [GET]
-app.get("/score", function (req, res) {
-  res.send(list);
-});
-
-//Endpoint CREATE - [POST]
-app.post("/score", function (req, res) {
-  //pega o item do formulário
-  const item = req.body;
-
-  //add item na lista
-  list.push({
-    id: list.length + 1,
-    name: item.name,
-    points: item.points,
+    res.send(item);
   });
 
-  res.status(200).send("OK");
-});
+  // [POST]
+  app.post("/score", async function (req, res) {
+    //pega o item do formulário
+    const item = req.body;
 
-app.listen(3000);
+    //add item na lista
+    await collection.insertOne(item);
+
+    res.status(200).send("OK");
+  });
+
+  // [PUT]
+  app.put("/score/:id", async function (req, res) {
+    const id = req.params.id;
+
+    collection.updateOne(
+      {
+        _id: new ObjectId(id),
+      },
+      {
+        $set: item,
+      }
+    );
+  });
+
+  app.listen(3000);
+}
+main();
