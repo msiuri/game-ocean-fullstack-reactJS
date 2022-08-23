6d6b1713b3862ef9b166d63d3d7ec6d6e0f64992
Iuri Moura <msiuri@outlook.com>
2022-08-23 09:21:43 -0300

    Adicionado novos itens do front

--- frontend/src/components/Game/Game.js
+++ frontend/src/components/Game/Game.js
@@ -1,10 +1,15 @@
 import "./Game.css";
 import clouds from "../../assets/clouds.png";
+import mario from "../../assets/mario.gif";
+import pipe from "../../assets/pipe.png";
 
 function Game() {
   return (
     <div className="game">
       <img className="clouds" src={clouds} alt="clouds" />
+      <img className="mario" src={mario} alt="mario" />
+      <img className="pipe" src={pipe} alt="pipe" />
+      <div className="floor"></div>
     </div>
   );
 }
