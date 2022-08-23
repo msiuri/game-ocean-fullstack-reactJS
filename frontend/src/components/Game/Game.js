0497f1308b1f5406a6f3bb21e6ab295cb25d0b34
Iuri Moura <msiuri@outlook.com>
2022-08-23 11:29:48 -0300

    Inserido função Jumping com iteração com entrada

--- frontend/src/components/Game/Game.js
+++ frontend/src/components/Game/Game.js
@@ -2,13 +2,33 @@ import "./Game.css";
 import clouds from "../../assets/clouds.png";
 import mario from "../../assets/mario.gif";
 import pipe from "../../assets/pipe.png";
+import { useState } from "react";
 
 function Game() {
+  console.log("Componente renderizado");
+  //Criamos o estado
+  const [jumping, setJumping] = useState(false);
+
+  document.onkeydown = function () {
+    //alteramos o estado para true
+    setJumping(true);
+    //reset state
+    setTimeout(() => {
+      setJumping(false);
+    }, 700);
+  };
+  let marioClassName = "mario";
+
+  if (jumping) {
+    marioClassName = "mario mario-jump";
+  }
+
+  console.log(15, { jumping });
+
   return (
     <div className="game">
       <img className="clouds" src={clouds} alt="clouds" />
-
-      <img className="mario mario-jump" src={mario} alt="mario" />
+      <img className={marioClassName} src={mario} alt="mario" />
       <img className="pipe" src={pipe} alt="pipe" />
       <div className="floor"></div>
     </div>
