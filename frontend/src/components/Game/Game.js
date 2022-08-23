ac0bbe35a1144d4a6f9da44c037187eabd6d3af0
Iuri Moura <msiuri@outlook.com>
2022-08-23 12:16:09 -0300

    Inserido lógica do Jumping

--- frontend/src/components/Game/Game.js
+++ frontend/src/components/Game/Game.js
@@ -2,12 +2,35 @@ import "./Game.css";
 import clouds from "../../assets/clouds.png";
 import mario from "../../assets/mario.gif";
 import pipe from "../../assets/pipe.png";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function Game() {
   console.log("Componente renderizado");
   //Criamos o estado
   const [jumping, setJumping] = useState(false);
+
+  //Criando as referencias
+  const marioRef = useRef();
+  const pipeRef = useRef();
+
+  function marioIsOnPipe() {
+    const mario = marioRef.current;
+    const pipe = pipeRef.current;
+
+    if (!mario || !pipe) {
+      return;
+    }
+    return (
+      pipe.offsetLeft > mario.offsetLeft &&
+      pipe.offsetLeft < mario.offsetLeft + mario.offsetWidth &&
+      mario.offsetTop + mario.offsetHeight > pipe.offsetTop
+    );
+  }
+
+  setInterval(() => {
+    const valor = marioIsOnPipe();
+    console.log("mario ta no cano", valor);
+  }, 100);
 
   document.onkeydown = function () {
     //alteramos o estado para true
@@ -28,8 +51,8 @@ function Game() {
   return (
     <div className="game">
       <img className="clouds" src={clouds} alt="clouds" />
-      <img className={marioClassName} src={mario} alt="mario" />
-      <img className="pipe" src={pipe} alt="pipe" />
+      <img ref={marioRef} className={marioClassName} src={mario} alt="mario" />
+      <img ref={pipeRef} className="pipe" src={pipe} alt="pipe" />
       <div className="floor"></div>
     </div>
   );
